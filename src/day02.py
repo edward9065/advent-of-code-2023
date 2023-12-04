df0636e390621e6a43120b4348bacc04ea57bbb1
Edward Chen <edward9065@gmail.com>
2023-12-04 16:51:03 -0500

    complete day 2 part 2

--- src/day02.py
+++ src/day02.py
@@ -1,34 +1,29 @@
-cube_count = {
-    "red": 12,
-    "green": 13,
-    "blue": 14,
-    "red\n": 12,
-    "green\n": 13,
-    "blue\n": 14,
-}
-# doing red\n is faster than stripping the string, for the cost of negligible space
+from functools import reduce
 
 total = 0
-
+cube_index = {
+    'red': 0,
+    'green': 1,
+    'blue': 2
+}
 with open('../input/day02.txt', encoding='utf-8') as input:
     for line in input:
-        game_data = line.split(": ")
+        game_data = line.strip().split(": ")
         draws = game_data[1].split("; ")
         
-        is_possible_game = True
+        # not the most readable but it's [red, green, blue]
+        max_count_list = [0, 0, 0]
 
         for draw in draws:
-            if(not is_possible_game):
-               break
             cubes = draw.split(", ")
             
             for cube in cubes:
                 cube_data = cube.split(" ")
-                if(cube_count[cube_data[1]] < int(cube_data[0])):
-                    is_possible_game = False
-                    break
+                max_count_list[cube_index[cube_data[1]]] = max(max_count_list[cube_index[cube_data[1]]], int(cube_data[0]))
+                
         
-        if(is_possible_game):
-            total += int(game_data[0][5:])
+        power = reduce(lambda x, y: x*y, max_count_list) #better to use math.prod probably but I wanted the practice
+        total += power
+
 
 print(total)
